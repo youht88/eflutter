1aea914a2f2cc8fa54a74b732d2401e688356af2
youht88 <1742716458@qq.com>
2021-04-28 21:34:03 +0800

    tobe continue

--- lib/pages/crypto.dart
+++ lib/pages/crypto.dart
@@ -2,25 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:cryptography/cryptography.dart';
+import 'dart:convert';
 
 class CryptoController extends GetxController {
   List<List<String>> info = [
     ["id", "youht"],
-    ["message", "I love flutter & dart!"],
     ["ec.publickey", ""],
     ["ec.privatekey", ""],
     ["rsa.publickey", ""],
-    ["rsa.privatekey", " "]
+    ["rsa.privatekey", " "],
+    ["message", "I love flutter & dart!"],
+    ["sha256(message)", ""],
+    ["sign message use ec", ""],
+    ["verify message use ec", ""],
+    ["sign message use rsa", ""],
+    ["verify message use rsa", ""],
+    ["passwd","123456"],
+    ["cipher message use ESA", ""],
+
   ];
   Widget itemBuilder(BuildContext context, int index) {
-    return Text("${info[index][0]}:${info[index][1]}");
-  }
-
-  Widget separatorBuilder(BuildContext context, int index) {
-    return Divider(
-      color: Colors.lightGreen,
-      thickness: 3,
+    return RichText(
+      text:TextSpan(
+        text:"${info[index][0]}:",
+        style:TextStyle(color:Colors.lightBlue,fontSize:12),
+        children:[TextSpan(text:"${info[index][1]}",style:TextStyle(color:Colors.deepOrange,))]
+      )
     );
+  }
+  Widget separatorBuilder(BuildContext context, int index) {
+    return (index==4 || index==6 || index==10)?Divider(
+      color: Colors.lightGreen,
+      thickness: 1.5,
+    ):Divider(thickness:0.0);
   } 
 
   @override
@@ -28,10 +42,9 @@ class CryptoController extends GetxController {
     super.onInit();
     var crypto = Ed25519();
     final keyPair = await crypto.newKeyPair();
-    print(keyPair);
+    info[2][1]=Encoding.ASCII.GetString(await keyPair.extractPrivateKeyBytes());
+    print(info);
   }
-  @override
-  void on
 }
 
 class CryptoPage extends GetView<CryptoController> {
